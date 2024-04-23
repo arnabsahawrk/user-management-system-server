d2af88f18e7bced4d10fa96ab25db6617b77c3c1
arnabw <arnabsahawrk@gmail.com>
2024-04-24 01:30:46 +0600

    GET, POST, DELETE and Deploy to Vercel

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+
+//config
+require("dotenv").config();
 const port = process.env.PORT || 8080;
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2p5zaxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const database = client.db("userManagementSystem").collection("users");
+
+    //GET
+    app.get("/users", async (req, res) => {
+      const result = await database.find().toArray();
+      res.send(result);
+    });
+
+    //POST
+    app.post("/users", async (req, res) => {
+      // console.log(req.body);
+      const result = await database.insertOne(req.body);
+      res.send(result);
+    });
+
+    //DELETE
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await database.deleteOne(query);
+      res.send(result);
+      // console.log(id);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(`Server is running on ${port}`);
